d5756a4b93838d5887939f6963cde740069d7aa0
Kamal Heib <kamalheib1@gmail.com>
2017-03-02 15:44:11 +0200

    Senty: tests: Implement RC pingpong test
    Implement RC pingpong test
    
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

---
+++ senty/tests/blocks/rdma/ibv_xx_pingpong.py
@@ -0,0 +1,57 @@
+#!/usr/bin/evn python
+
+"""
+Senty Project
+Copyright(c) 2017 Senty.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms and conditions of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The full GNU General Public License is included in this distribution in
+the file called "COPYING".
+
+Contact Information:
+Kamal Heib <kamalheib1@gmail.com>
+"""
+
+
+from senty.tests.blocks.common.traffic import Traffic
+
+
+class IBvXXPingPong(Traffic):
+    def __init__(self, logger, server, client, con_type, s_dev, c_dev, s_gid, c_gid, s_args='', c_args=''):
+        super(IBvXXPingPong, self).__init__(logger=logger, server=server, client=client)
+        self._con_type = con_type
+        self._s_dev = s_dev
+        self._c_dev = c_dev
+        self._s_gid = s_gid
+        self._c_gid = c_gid
+        self._s_args = ("", s_args)[s_args is not None]
+        self._c_args = ("", c_args)[c_args is not None]
+
+    def get_server_command(self):
+        return "ibv_%s_pingpong -d %s -g %s %s" % (self._con_type, self._s_dev, self._s_gid, self._s_args)
+
+    def get_client_command(self):
+        return "ibv_%s_pingpong -d %s -g %s %s %s" % (self._con_type, self._c_dev, self._c_gid, self._c_args, self.Server.IP)
+
+    def init(self):
+        self.Logger.pr_dbg('--------=== Initialization stage - [ %s ] ===--------' % self.__class__.__name__)
+
+    def wait(self):
+        self.Logger.pr_dbg('--------=== Wait stage - [ %s ] ===--------' % self.__class__.__name__)
+        (rc, out) = self.Client.wait_process(self._host_to_pid[self.Client])
+        if rc:
+            self.Logger.pr_err('%s - %s' % (self.Client.IP, out))
+
+        super(IBvXXPingPong, self).kill()
+        return rc
---
+++ senty/tests/rdma/ibv_rc_pingpong/ibv_rc_pingpong.py
@@ -0,0 +1,71 @@
+#!/usr/bin/evn python
+
+"""
+Senty Project
+Copyright(c) 2017 Senty.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms and conditions of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The full GNU General Public License is included in this distribution in
+the file called "COPYING".
+
+Contact Information:
+Kamal Heib <kamalheib1@gmail.com>
+"""
+
+import sys
+from senty.tests.common.traffic_test import TrafficTest
+from senty.tests.blocks.rdma.ibv_xx_pingpong import IBvXXPingPong
+
+
+class IBvRCPingPong(TrafficTest):
+    def __init__(self):
+        super(IBvRCPingPong, self).__init__()
+        self.caseToTests = {}
+
+    def init_tests(self):
+        for case in self.Cases:
+            tests = []
+            for s_dev, c_dev in self.RDMAPairs.iteritems():
+                for s_port in s_dev.Ports:
+                    c_port = self.get_pair(s_port, c_dev.Ports)
+                    for s_gid in s_port.V1GIDs:
+                        c_gid = c_port.V1GIDs[s_port.V1GIDs.index(s_gid)]
+                        tests.append(IBvXXPingPong(self.Logger, self.Server, self.Client, "rc", s_dev.Name, c_dev.Name,
+                                                   s_gid, c_gid, case.ServerArgs, case.ClientArgs))
+                    for s_gid in s_port.V2GIDs:
+                        c_gid = c_port.V2GIDs[s_port.V2GIDs.index(s_gid)]
+                        tests.append(IBvXXPingPong(self.Logger, self.Server, self.Client, "rc", s_dev.Name, c_dev.Name,
+                                                   s_gid, c_gid, case.ServerArgs, case.ClientArgs))
+            self.caseToTests[case] = tests
+
+    def setup(self):
+        super(IBvRCPingPong, self).setup()
+        for case, tests in self.caseToTests.iteritems():
+            [test.init() for test in tests]
+        return 0
+
+    def run(self):
+        super(IBvRCPingPong, self).run()
+        rcs = [0]
+        for case in self.Cases:
+            self.Logger.pr_info("%s" % case.Summary)
+            tests = self.caseToTests[case]
+            for test in tests:
+                rcs += [test.run()]
+        return sum(rcs)
+
+if __name__ == "__main__":
+    test = IBvRCPingPong()
+    rc = test.execute(sys.argv[1:])
+    sys.exit(rc)
